6c4669e258b97b161ed185be4a5661020a7ca37c
kirth8 <alexnirth@gmail.com>
2025-11-13 18:24:52 -0500

    Se añade estilos para la pagina principal de home v1

--- helloportafolio/src/components/Prueba.js
+++
@@ -1,7 +0,0 @@
-export default function Complemento() {
-    return(
-        <div style={{ textAlign: "center", marginTop: "50px" }}> 
-            <h1>Las cristianas lo mas duro del sistema.</h1>
-        </div>
-    );
-}
--- helloportafolio/src/pages/Home.js
+++ helloportafolio/src/pages/Home.js
@@ -1,3 +1,27 @@
+import React from "react";
+import styles from "../components/css/Home.module.css"
+import miFoto from "../assets/perfil.png"
+
 export default function Home() {
-  return <h2>Home</h2>;
+  return (
+    <div className= {styles.container}>
+
+      <section className= {styles.hero}>
+          <h1 className= {styles.title}>¡Hello!, I'm Keyner Manchay</h1>
+      </section>
+      
+      <div>
+        <img src={miFoto} alt="Mi foto" className={styles.fotoPerfil} />
+      </div>
+
+      <section className= {styles.content}>
+          <p>Programador full stack con dos años de experiencia en el desarrollo 
+            y diseño de aplicaciones web dinámicas y escalables. Conocimiento en 
+            tecnologías como React, Node.js, Flask, Django y bases de datos 
+            SQL/NoSQL. En constante aprendizaje y comprometido con seguir las
+             mejores prácticas de desarrollo. </p>
+      </section>
+    </div>
+
+  );
 }

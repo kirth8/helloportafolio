7097549221f6e640ad86ffe45637e8c47a0b32c7
kirth8 <alexnirth@gmail.com>
2025-11-13 23:05:04 -0500

    Se añadio unas mejoras a la pagina home con un apartado de Tecnologías utilizadas

--- helloportafolio/src/pages/Home.js
+++ helloportafolio/src/pages/Home.js
@@ -4,23 +4,68 @@ import miFoto from "../assets/perfil.png"
 
 export default function Home() {
   return (
+    
     <div className= {styles.container}>
 
       <section className= {styles.hero}>
           <h1 className= {styles.title}>¡Hello!, I'm Keyner Manchay</h1>
+        
+          <img src={miFoto} alt="Mi foto" className={styles.fotoPerfil} />
+        
       </section>
       
-      <div>
-        <img src={miFoto} alt="Mi foto" className={styles.fotoPerfil} />
-      </div>
+      
 
       <section className= {styles.content}>
-          <p>Programador full stack con dos años de experiencia en el desarrollo 
+          <p>Programador full stack junior con dos años de experiencia en el desarrollo 
             y diseño de aplicaciones web dinámicas y escalables. Conocimiento en 
             tecnologías como React, Node.js, Flask, Django y bases de datos 
             SQL/NoSQL. En constante aprendizaje y comprometido con seguir las
              mejores prácticas de desarrollo. </p>
       </section>
+      <section className={styles.habilidades}>
+        <h2>Tecnologías</h2>
+
+        <div className={styles.gridHabilidades}>
+          <div className={styles.skill}>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="React" />
+            <p>React</p>
+          </div>
+          <div className={styles.skill}>
+             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="Node.js" />
+            <p>Node.js</p>
+          </div>
+          <div className={styles.skill}>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt="MongoDB" />
+            <p>MongoDB</p>
+          </div>
+          <div className={styles.skill}>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original.svg" alt="Flask" />
+            <p>Flask</p>
+          </div>
+          <div className={styles.skill}>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg" alt="Django" />
+            <p>Django</p>
+          </div>
+          <div className={styles.skill}>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" alt="Docker" />
+            <p>Docker</p>
+          </div>
+          <div className={styles.skill}>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" />
+            <p>GitHub</p>
+          </div>
+          <div className={styles.skill}>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg" alt="Azure" />
+            <p>Azure</p>
+          </div>
+          <div className={styles.skill}>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" alt="MySQL" />
+            <p>MySQL</p>
+          </div>
+        </div>
+      </section>
+
     </div>
 
   );

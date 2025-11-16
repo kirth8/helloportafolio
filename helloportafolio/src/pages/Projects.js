71dbe3ceee60f274e1c34466b1016171f1b91af0
kirth8 <alexnirth@gmail.com>
2025-11-16 00:03:18 -0500

    Se plasman las propuestas de contenido para la pagina de proyectos

--- helloportafolio/src/pages/Projects.js
+++ helloportafolio/src/pages/Projects.js
@@ -1,3 +1,9 @@
 export default function Projects() {
   return <h2>Projectos</h2>;
+    // <p>0. Sistema de Gestión de ventas </p>  
+    // <p>1. Manjilu</p>  
+    // <p>2. Lector Accesibilidad</p>
+    // <p>3. Menu de servicios Comunitarios</p>
+    // <p>4. InstantSale</p>
+    // <p>5. Trabajo Autonomo 3</p>
 }
